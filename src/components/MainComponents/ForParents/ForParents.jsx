327c191ba4ad76936e1ed074af7fe2a81aef3141
Filipov <andrijfilipov73@gmail.com>
2023-08-20 20:54:37 +0300

    add next section for parents

--- src/components/MainComponents/ForParents/ForParents.jsx
+++ src/components/MainComponents/ForParents/ForParents.jsx
@@ -1,4 +1,33 @@
+import { Button } from '@mui/material'
+import './ForParents.scss'
+import love_family from 'img/love_family.svg'
+
 const ForParents = () => {
-    return <section className="for_parents">12412412</section>
+    return (
+        <section className="for_parents">
+            <div className="main_container">
+                <div className="parents_info">
+                    <h3>Довідник для батьків</h3>
+                    <p>
+                        <span>
+                            Цікавий та легкий у користуванні банк для дітей
+                        </span>
+                        - це найкращий вибір для ваших найрідніших. Допоможіть
+                        своїм дітям дізнатися цей чудовий шлях до
+                        фундаментальних понять заощадження, витрачання та
+                        інвестування грошей.
+                    </p>
+                    <Button color="error" variant="contained">
+                        Дізнатися більше
+                    </Button>
+                </div>
+                <img
+                    className="picture_family"
+                    src={love_family}
+                    alt="love family"
+                />
+            </div>
+        </section>
+    )
 }
 export default ForParents
